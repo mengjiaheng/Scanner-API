1b29c57f12958b48e6c4a50fdae851c9ab0b7c5f
mengjiaheng <mengjiaheng@asants.com>
2021-08-25 16:19:13 +0800

    feat 封装request请求

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"github.com/mengjiaheng/scanapi/config"
-	"github.com/mengjiaheng/scanapi/service"
-)
-
 func main() {
-	cf := config.Default()
-	service.RequestUrl(cf.TargetUrl)
+
 }
