92a58f2a04521d2a28e227149419dbaf57e1ea8c
mengjiaheng <mengjiaheng@asants.com>
2021-08-25 17:26:29 +0800

    refactor 添加注释

--- utils/file/file.go
+++ utils/file/file.go
@@ -7,28 +7,33 @@ import (
 	"path"
 )
 
+// 获取文件大小
 func GetSize(f multipart.File) (int, error) {
 	content, err := ioutil.ReadAll(f)
 
 	return len(content), err
 }
 
+// 获取文件后缀
 func GetExt(fileName string) string {
 	return path.Ext(fileName)
 }
 
+// 检查文件是否存在
 func CheckNotExist(src string) bool {
 	_, err := os.Stat(src)
 
 	return os.IsNotExist(err)
 }
 
+// 检查文件权限
 func CheckPermission(src string) bool {
 	_, err := os.Stat(src)
 
 	return os.IsPermission(err)
 }
 
+// 如果不存在则新建文件夹
 func IsNotExistMkDir(src string) error {
 	if notExist := CheckNotExist(src); notExist == true {
 		if err := MkDir(src); err != nil {
@@ -39,6 +44,7 @@ func IsNotExistMkDir(src string) error {
 	return nil
 }
 
+// 创建目录
 func MkDir(src string) error {
 	err := os.MkdirAll(src, os.ModePerm)
 	if err != nil {
@@ -48,6 +54,7 @@ func MkDir(src string) error {
 	return nil
 }
 
+// 打开文件
 func Open(name string, flag int, perm os.FileMode) (*os.File, error) {
 	f, err := os.OpenFile(name, flag, perm)
 	if err != nil {
