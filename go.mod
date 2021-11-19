53c879564dcbf49f373cc94c94e1267fd9f18cc2
孟家恒 <mengjiaheng@asants.com>
2021-11-19 11:51:26 +0800

    feat: 新增gin

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mengjiaheng/scanapi
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/gin-gonic/gin v1.7.4
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	r := gin.Default()
+	r.GET("/xml", func(c *gin.Context) {
+		c.XML(200, gin.H{"wechat": "flysnow_org", "blog": "www.flysnow.org"})
+	})
+	r.POST("/upload", func(c *gin.Context) {
+		c.JSON(200, "ok")
+	})
+	r.Run(":8080")
 }
