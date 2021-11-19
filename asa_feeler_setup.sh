af0d746f6d8b8b2db48d2e3220a48b006a75ffd8
孟家恒 <mengjiaheng@asants.com>
2021-11-19 17:39:10 +0800

    doc: 修改shell文件

--- asa_feeler_setup.sh
+++ asa_feeler_setup.sh
@@ -9,5 +9,5 @@ read -p "请输入服务启动端口：" ASA_FEELER_SERVER_PORT
 sed -i "s/{ASA_FEELER_SERVER_IP}/${ASA_FEELER_SERVER_IP}/g" /opt/asa_feeler/Scanner-API/config/config.yaml
 sed -i "s/{ASA_FEELER_SERVER_PORT}/${ASA_FEELER_SERVER_PORT}/g" /opt/asa_feeler/Scanner-API/config/config.yaml
 go build -o scanner
-./scanner
+nohup ./scanner > log.log &
 
